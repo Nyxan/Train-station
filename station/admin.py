f415edd2cd93572a681b46208656d0a98d0b3ce5
Oleksandr Moroz <molecxander@gmail.com>
2024-06-08 21:42:38 +0300

    add improved admin

--- station/admin.py
+++ station/admin.py
@@ -2,11 +2,21 @@ from django.contrib import admin
 
 from station.models import Route, Ticket, Train, TrainType, Station, Crew, Order, Journey
 
+
+class TicketInline(admin.TabularInline):
+    model = Ticket
+    extra = 1
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    inlines = [TicketInline]
+
+
 admin.site.register(Route)
 admin.site.register(Train)
 admin.site.register(TrainType)
 admin.site.register(Station)
 admin.site.register(Crew)
-admin.site.register(Order)
 admin.site.register(Journey)
 admin.site.register(Ticket)
