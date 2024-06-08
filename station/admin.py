c79f839151e1fe731ef64265330a2309859b2fd4
Oleksandr Moroz <molecxander@gmail.com>
2024-06-08 15:07:17 +0300

    add admin for user & station, create user model, add apps to settings & urls

--- station/admin.py
+++ station/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from station.models import Route, Ticket, Train, TrainType, Station, Crew, Order, Journey
+
+admin.site.register(Route)
+admin.site.register(Train)
+admin.site.register(TrainType)
+admin.site.register(Station)
+admin.site.register(Crew)
+admin.site.register(Order)
+admin.site.register(Journey)
+admin.site.register(Ticket)
